ce14c6858727cc3d05d77b7b758df92cd54e5557
NairaTahira <naira.tahira9a@gmail.com>
2025-11-19 14:54:09 +0700

    fix: fixing bku history design

--- resources/js/utils/getSidebarLinks.js
+++ resources/js/utils/getSidebarLinks.js
@@ -54,7 +54,7 @@ export default function getSidebarLinks(role) {
           component: "Dashboards/DirekturDashboard"
         },
         {
-          name: "Persetujuan",
+          name: "Daftar Persetujuan",
           route: "direktur.daftarpersetujuan",
           icon: "square-check",
           component: "Direktur/DaftarPersetujuan"
